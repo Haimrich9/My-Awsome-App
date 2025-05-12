3056921a1f8363c783bf5020bf1e55d0b5826fb3
Haimrich9 <hekr99@gmail.com>
2025-05-12 23:43:05 +0200

    updated

--- src/app.js
+++ src/app.js
@@ -1,38 +1,31 @@
 function updateCity() {
-  let currentCity = document.querySelector("h1");
-  let inputCity = document.querySelector("#inputCity");
-  inputCity = inputCity.value;
-  let city = inputCity;
-  let inputCityFirstLetter = inputCity.charAt(0).toUpperCase();
-  inputCity = inputCity.slice(1).toLowerCase();
-  currentCity.innerHTML = inputCityFirstLetter + inputCity;
-  let apiKey = "f8f8b2941t5850dac0b4334a68o053a6";
-  let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-  axios.get(url).then(showUser);
+	let currentCity = document.querySelector("h1");
+	let inputCity = document.querySelector("#inputCity");
+	inputCity = inputCity.value;
+	let city = inputCity;
+	let inputCityFirstLetter = inputCity.charAt(0).toUpperCase();
+	inputCity = inputCity.slice(1).toLowerCase();
+	currentCity.innerHTML = inputCityFirstLetter + inputCity;
+	let apiKey = "f8f8b2941t5850dac0b4334a68o053a6";
+	let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+	axios.get(url).then(showUser);
 }
 
 function showUser(response) {
-  let temp = document.querySelector(".temp");
-  let emoji = document.querySelector(".emoji");
-  let city = document.querySelector("h1");
-  temp.innerHTML = Math.round(response.data.temperature.current);
-  document
-    .querySelector("#icon")
-    .setAttribute("src", response.data.condition.icon_url);
+	let temp = document.querySelector(".temp");
+	let description = document.querySelector("#description");
+	let windSpeed = document.querySelector("#wind-speed");
+	description.innerHTML = response.data.condition.description;
+	windSpeed.innerHTML = `Wind: ${Math.round(response.data.wind.speed)} km/h`;
+	let humidity = document.querySelector("#humidity");
+	humidity.innerHTML = `Humidity: ${Math.round(response.data.temperature.humidity)}%`;
+	temp.innerHTML = Math.round(response.data.temperature.current);
+	document.querySelector("#icon").setAttribute("src", response.data.condition.icon_url);
 }
 
 let now = new Date();
 
-let days = [
-  "Sunday",
-  "Monday",
-  "tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-  "Sunday",
-];
+let days = ["Sunday", "Monday", "tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 let day = days[now.getDay()];
 let hours = now.getHours();
 let minutes = now.getMinutes();
